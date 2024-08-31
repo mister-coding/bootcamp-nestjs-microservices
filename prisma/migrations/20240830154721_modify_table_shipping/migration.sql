309d5c16d258ebfbe366786273110fbb46edb2b7
agik-setiawan <setiawanagik@gmail.com>
2024-09-01 00:23:27 +0700

    ddd

---
+++ prisma/migrations/20240830154721_modify_table_shipping/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "shipping" ADD COLUMN     "adm_notes" TEXT,
+ADD COLUMN     "courier_name" TEXT,
+ADD COLUMN     "courier_phone" TEXT,
+ADD COLUMN     "notes" TEXT,
+ADD COLUMN     "shipping_no" TEXT;
